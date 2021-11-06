be0e3389e53ce12e23730ab063cfbf6a1b14749d
Gilson <gilsontorresr@hotmail.com>
2021-11-06 16:02:03 -0300

    ok

--- src/controller/admController.js
+++ src/controller/admController.js
@@ -1,5 +1,10 @@
 import db from '../db.js'
 
+import crypto from 'crypto-js'
+
+import  Sequelize  from 'sequelize';
+const { Op, col, fn } = Sequelize;
+
 import express from 'express'
 const Router = express.Router
 const app = Router();
@@ -13,21 +18,68 @@ app.get('/', async (req, resp) => {
             ]
         });
         resp.send(r);
+
     } catch (e) {
         resp.send({ erro: e.toString() })
     }
 })
 
 
-app.post('/', async (req, resp) => {
+app.post('/login', async (req, resp) => {
     try {
         
         let { email, senha } = req.body;
+        let cryptoSenha = crypto.SHA256(senha).toString(crypto.enc.Base64);
+        
+
+        let r = await db.infod_ssc_adm.findOne(
+            {
+                where: {
+                    ds_email: email,
+                    ds_senha: cryptoSenha
+                },
+                raw: true
+            }
+        )
+
+        if (email === "" || senha === "") {
+            return resp.send({ erro: 'Preencha todos os campos!' });
+        }
+
+        if (!r) {
+            return resp.send({ erro: 'Credenciais inválidas.' })
+        }
+    
+        delete r.ds_senha;
+        resp.send(r);
+    
+    } catch(b) {
+        resp.send({ erro: b.toString() })
+    }
+})
+
+
+app.post('/cadastro', async (req, resp) => {
+    try {
+        
+        let  { email, senha } = req.body;
+
+        let cryptoSenha = crypto.SHA256(senha).toString(crypto.enc.Base64);
+
+        if (email === "" || senha === "") {
+            return resp.send({ erro: 'Preencha todos os campos!' });
+        }
+
+        if (!email.includes('@')) {
+            return resp.send({ erro: 'Insira um email válido'})
+        }
+        
 
         let b = await db.infod_ssc_adm.create({
             ds_email: email,
-            ds_senha: senha
+            ds_senha: cryptoSenha
         })
+
         resp.send(b);
     
     } catch(b) {
